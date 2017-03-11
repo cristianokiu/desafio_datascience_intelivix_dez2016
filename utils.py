14993acec6cd7ee6282425570e69f84f2f2cd216
Paulo Romeira <paulo@pauloromeira.com>
2017-03-11 20:40:26 -0300

    Modify plot_confusion_matrix from utils to use subplot

--- utils.py
+++ utils.py
@@ -8,27 +8,36 @@ import matplotlib.pyplot as plt
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='Confusion matrix',
-                          cmap=plt.cm.Greys):
+                          cmap=plt.cm.Greys,
+                          subplot=None):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
     """
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
+
+    if not subplot:
+        subplot = plt.subplot()
+
+    subplot.imshow(cm, interpolation='nearest', cmap=cmap)
+    subplot.set_title(title)
     tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+
+    subplot.set_xticks(tick_marks)
+    subplot.set_xticklabels(classes)
+    for tick in subplot.get_xticklabels():
+        tick.set_rotation(45)
+
+    subplot.set_yticks(tick_marks)
+    subplot.set_yticklabels(classes)
 
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
+        subplot.text(j, i, cm[i, j],
                  horizontalalignment="center",
                  color="white" if cm[i, j] > thresh else "black")
 
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+    subplot.set_ylabel('True label')
+    subplot.set_xlabel('Predicted label')
